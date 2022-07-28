104d1f4d05537fc2b198358eac93839360bc7cc8
Joao Charly Meneces Paredes <charlymeneces6@gmail.com>
2022-07-28 03:56:21 -0400

    modified login page

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -7,18 +7,29 @@ import { AbstractControlOptions, FormBuilder, FormGroup, Validators } from '@ang
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  formSignup!: FormGroup;
+  formSignUp!: FormGroup;
+  formSignIn!: FormGroup;
 
   constructor(private fb: FormBuilder) {
-    this.createFormSignup()
+    this.createFormSignIn();
+    this.createFormSignUp;
   }
 
   ngOnInit(): void {
   }
 
-  createFormSignup(): void {
-    this.formSignup = this.fb.group({
-      userName: ['', [Validators.required, Validators.minLength(4), Validators.pattern(/^[a-zA-zñÑ\s]+$/)]],
+  createFormSignIn(): void {
+    this.formSignIn = this.fb.group({
+      ci: ['', [Validators.required, Validators.minLength(7), Validators.maxLength(7)]],
+      password: ['', [Validators.required]]
+    },{
+      validador: Validators.required
+    } as AbstractControlOptions);
+  }
+
+  createFormSignUp(): void {
+    this.formSignUp = this.fb.group({
+      ci: ['', [Validators.required, Validators.minLength(4), Validators.pattern(/^[a-zA-zñÑ\s]+$/)]],
       password: ['', [Validators.required]]
     },{
       validador: Validators.required
@@ -29,6 +40,13 @@ export class LoginComponent implements OnInit {
     console.log("registrando");
     setTimeout(() => {
       console.log("enviado");
+    }, 2000);
+  }
+
+  signIn() {
+    console.log('ingresando');
+    setTimeout(() => {
+      console.log('Dentro');
     }, 2000);
   }
 }
