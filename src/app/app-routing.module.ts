ddb3a397ec880e333cfe03619ed4f64ffb658ab2
Joao-123 <charlymeneses7@gmail.com>
2022-07-28 03:00:11 -0400

    add login and not-found page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from "./components/login/login.component";
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  //{ path: 'second-component', component: SecondComponent },
+  { path: '',   redirectTo: '/login', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControlOptions, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  formSignup!: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {
+    this.createFormSignup()
+  }
 
   ngOnInit(): void {
   }
 
+  createFormSignup(): void {
+    this.formSignup = this.fb.group({
+      userName: ['', [Validators.required, Validators.minLength(4), Validators.pattern(/^[a-zA-zñÑ\s]+$/)]],
+      password: ['', [Validators.required]]
+    },{
+      validador: Validators.required
+    } as AbstractControlOptions);
+  }
+
+  register() {
+    console.log("registrando");
+    setTimeout(() => {
+      console.log("enviado");
+    }, 2000);
+  }
 }
